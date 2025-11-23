73102f09d470f7db8b0bc85bac9738f0d32a2f33
Gerardo Javier <jerryoscicon@gmail.com>
2025-11-22 22:16:07 -0600

    variable de coeficiente agregado
    Añadí variable de coeficiente para calcular volumen

--- calculo_agua.py
+++ calculo_agua.py
@@ -5,6 +5,7 @@ techo_area = 4297 # m2
 lluvia_anual = 583 # mm
 
 def calcular_volumen(area, lluvia):
+    coeficiente = 0.9 # Cambio hecho desde la WEB
     return area * lluvia
 
 print(f"Volumen total: {calcular_volumen(techo_area, lluvia_anual)} litros")
